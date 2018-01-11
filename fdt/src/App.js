c9f45ff374f58efb2089fc65cfb7ff1b166ca7ce
Stef <seedsofthesesame@gmail.com>
2018-01-11 12:59:57 -0800

    Can add glucose numbers.

--- fdt/src/App.js
+++ fdt/src/App.js
@@ -1,21 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="App-header">
+
+      <h1 className="App-title">Feline Diabetes Tracker</h1>
+      </header>
+      <p className="App-intro">
+      Welcome to the Feline Diabetes Tracker!
+      </p>
+      <GlucoseInput/>
       </div>
     );
   }
 }
+
+class GlucoseInput extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      glucose: 100
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Add Glucose Level!
+        <input
+          type="number"
+          onChange={(event) => {
+            this.setState({glucose: event.target.value})
+          }
+        }
+        value={this.state.glucose}
+        />
+        <button
+          onClick={() => {
+            console.log(this.state);
+            fetch('http://localhost:3000/glucose', {
+              method: 'POST',
+              body: JSON.stringify({
+                glucose: this.state.glucose
+              })
+            })
+          }}
+        >Add!</button>
+      </div>
+    )
+  }
+}
+
 
 export default App;
